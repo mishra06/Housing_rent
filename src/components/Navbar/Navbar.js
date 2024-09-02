331c68fc1c655645d97b535f232c24d5f6e90cfb
mishra06 <mishra.vibhakar7262@gmail.com>
2024-09-02 16:57:55 +0530

    "media query for movile view"

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -43,22 +43,22 @@ const Navbar = () => {
   
   
   return (
-    <div className='flex justify-evenly items-center h-[15vh] w-full'>
+    <div className=' navbar_sec flex justify-evenly items-center h-[15vh] w-full'>
       <div>
         <HiMiniHome onClick={() => navigate("/")} size={30} className='cursor-pointer text-green-800' />
       </div>
-      <div >
+      <div className='navbar_second_sec'>
         <h1 className='text-4xl font-medium capitalize text-red-800'>Search properties to rent</h1>
       </div>
-      <div className=' flex justify-center-items-center gap-2'>
-        <div className='flex gap-1'>
+      <div className='navbar_third_sec flex justify-center-items-center gap-2'>
+        <div className='navbar_third_first flex gap-1'>
             <input
             value={search}
             onChange={(e)=>{setSearch(e.target.value)}}
              className='border-2 border-solid border-gray p-1 placeholder-gray-500 rounded px-2 bg-[#f1f7ff] ' placeholder="Search by name and city" type="text" />
             <button onClick={submitHandeler} className='border-2 border-solid border-black px-4 py-1 rounded-[8px] outline-none hover:text-white hover:bg-black'>Search</button>
         </div>
-        <div>
+        <div className='navbar_submit_buttom'>
             <button onClick={()=>navigate("/fab")} className='border-2 border-solid border-black px-4 py-1 rounded-[8px] outline-none hover:text-white hover:bg-black'>Likes</button>
         </div>
       </div>
